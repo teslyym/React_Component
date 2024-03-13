c6f09482abf591f3242c68ebd393b8e4bec02a11
Teslim <steslimolayiwola@gmail.com>
2024-03-13 14:18:15 +0100

    addition of product components

---
+++ src/Image.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import product from './product'
+
+const Image = () => {
+  return (
+    <div>{product.Image}</div>
+  )
+}
+
+export default Image
--- src/product.js
+++ src/product.js
@@ -1,6 +1,8 @@
-const data={
+const product={
     name:"Food",
     price:"$17",
     description:"I seel rice and chicken sauce",
     path: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DBZbZbwWLCKE&psig=AOvVaw0DSyebTiKJl-dld-PWJ8no&ust=1710420939901000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKjy29ak8YQDFQAAAAAdAAAAABAE"
 }
+
+export default product
